800c39fea87e3917a2e9d2d3016d36a443b4c753
Schwartz, Blake <Blake.H.Schwartz.-ND@disney.com>
2017-06-24 13:56:28 -0700

    ex 4 (done out of order)

--- adv-js/exercises/ex4/ex4.js
+++ adv-js/exercises/ex4/ex4.js
@@ -13,29 +13,31 @@ Widget.prototype.render = function($where){
 	}
 };
 
-function Button(/* ... */) {
-/*
-	...
+function Button(width, height, label) {
+	// super call (Widget, parent object)
+	Widget.call(this, width, height);
+	this.label = label || 'Default';
 	this.$elem = $("<button>").text(this.label);
-*/
 }
+// instantiate Button with linkage to Widget
+Button.prototype = Object.create(Widget.prototype);
 
-/*
-Button -> render = function($where) {
-	// call the parent render()
+Button.prototype.render = function($where) {
+	// call the parent render() ('super' call)
+	Widget.prototype.render.call(this, $where);
 	// add a click handler -> onClick
-}
+	this.$elem.click(this.onClick.bind(this));
+};
 
-Button -> onClick = function(evt) {
-	console.log("...");
+Button.prototype.onClick = function(evt) {
+	console.log(`Button ${this.label} clicked!`);
 }
 
 $(document).ready(function(){
 	var $body = $(document.body);
-	var btn1 = ...;
-	var btn2 = ...;
+	var btn1 = new Button(130, 35, 'Stuff');
+	var btn2 = new Button(150, 45, 'Other');
 
 	btn1.render($body);
 	btn2.render($body);
 });
-*/
