e48d95094cd337cf17e99d3d14f5d8e95dc6999c
Schwartz, Blake <Blake.H.Schwartz.-ND@disney.com>
2017-06-24 21:01:22 -0700

    ex1 and 2

--- functional-light-programming/exercises/ex1/ex1.js
+++ functional-light-programming/exercises/ex1/ex1.js
@@ -1,12 +1,17 @@
-function foo(x) {
-	y++;
-	z = x * y;
+function bar(x, y) {
+	var z;
+	foo(x);
+	return [y, z];
+
+	function foo(x) {
+		y++;
+		z = x * y;
+	}
+
 }
 
-var y = 5, z;
+bar(20, 5);
+z; // 120
 
-foo(20);
-z;		// 120
-
-foo(25);
-z;		// 175
+bar(25, 6);
+z; // 175
