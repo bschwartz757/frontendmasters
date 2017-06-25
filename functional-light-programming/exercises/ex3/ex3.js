87e01329941f93b724f14e73526b14801aaadb8d
Schwartz, Blake <Blake.H.Schwartz.-ND@disney.com>
2017-06-24 21:57:26 -0700

    adds ex3 (recursion example)

--- functional-light-programming/exercises/ex3/ex3.js
+++ functional-light-programming/exercises/ex3/ex3.js
@@ -1,5 +1,11 @@
-function mult(x,y,z) {
-	return x * y * z;
+function mult(...args) {
+	// base case - where we exit the recursion
+	if (args.length <= 2) {
+		return args[0] * args[1];
+	}
+	// until we reach base case - call the function again,
+	// passing the next value in the list
+	return args[0] * mult(...args.slice(1));
 }
 
 mult(3,4,5);	// 60
